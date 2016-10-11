bd323d319dd77a6f5bf754222053ad4f99767b75
Thanawat <pipethegod@gmail.com>
2016-10-11 22:34:40 +0700

    add login

---
+++ web/resources/controller/index.js
@@ -0,0 +1,38 @@
+angular.module('csGeneration',[
+    "ui.router",
+    "ui.bootstrap",
+    "loginModule",
+])
+.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.
+        state('login', {
+            url: '/login',
+            templateUrl: './resources/views/login.html',
+            controller: 'LoginCtrl'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: './resources/views/register.html',
+            controller: 'RegisterCtrl'
+        })
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: './resources/views/dashboard.html',
+            controller: 'DashboardCtrl'
+        });
+})
+.controller('indexController', function ($scope, $state, userService) {
+    $state.go('login');
+})
+.service('userService', function () {
+    var user = {};
+    this.setUser = function (info) {
+        user.access = info.access;
+        user.role = info.role;
+        user.profiles = info.profiles;
+    };
+    this.getUser = function () {
+        return user;
+    };
+});
